8ff15b63e06c85357e31c48a57f17cd1f46cdc6f
Kamini Pise <kaminipise0035@gmail.com>
2025-02-10 00:35:31 +0530

    Assignment

---
+++ SeleniumProjectNew2/src/test/java/SeleniumContactsPage/ContactsTest.java
@@ -0,0 +1,9 @@
+package SeleniumContactsPage;
+
+public class ContactsTest {
+
+	public static void main(String[] args) {
+		System.out.println("Contacts page created");
+	}
+
+}
---
+++ SeleniumProjectNew2/src/test/java/SeleniumHomePage/HomePageTest.java
@@ -0,0 +1,9 @@
+package SeleniumHomePage;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		System.out.println("Home page created");
+	}
+
+}
